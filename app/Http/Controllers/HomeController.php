b435b4426701749a414b6c4bf4753600e2d04d1b
Anthony <anthony.gonzalez@3wa.io>
2024-07-17 00:16:25 +0200

    ok storage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,17 +8,6 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         $projects = Projects::selectRaw('*,FALSE AS show_description')->with('documents')->get();
