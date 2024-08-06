cbc9789f8a8b1807c61769e5cf0ce799f8055d3c
agonzalez <agonzalez@planfor.fr>
2024-08-06 15:05:48 +0200

    score recap

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -85,7 +85,7 @@ class HomeController extends Controller
         try {
             $captchaResult = HomeController::verifyCaptcha($captcha);
     
-            if (!$captchaResult['success'] || $captchaResult['score'] < 0.4) {
+            if (!$captchaResult['success'] || $captchaResult['score'] < 0.2) {
                 return response()->json(['statut' => 'error', 'message' => 'Erreur CAPTCHA ('.$captchaResult['score'].')'], 500);
             }
     
